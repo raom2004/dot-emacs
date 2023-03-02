a0b2ec6fb93893c25ea67e535e76aacb896e049c
Ricardo <raom2004@gmail.com>
2023-03-02 15:30:46 +0100

    open Project folder at startup

--- init-openbox.el
+++ init-openbox.el
@@ -1,64 +1,60 @@
-;;;~ init-openbox.el
-;;;~
+;; init-openbox.el -- Emacs init file -*- lexical-binding: t -*-
+;;
+;;;; ====================== HEADER =========================
+;;
+;; Title: Emacs Init File with a list of files to open at startup
+;; Author: Ricardo Orbegozo
+;; Created: 2020-04-13
+;; Updated: 2023-02-14 20:11:38
+;; Source: init-openbox.org
+;;
 ;;;~ Description:
 ;;;~  emacs init file that include other relevant dot-files,
 ;;;~  open and fullfil the screen with three buffers,
 ;;;~  and finally set which files will be shown in each buffer
 
+
 ;;;~ FRAME 1: load custom init file: user-init-file
 (progn
   (org-mode)
-  (setq user-init-file "~/Projects/dot-emacs/init-openbox.el")
-  ;; (if (not (file-exists-p user-init-file))
-  ;;     (shell-command "xterm -rv -e \"cd ~/Projects/dot-emacs && bash bootstrap && echo '==> emacs bootstrap SUCCESSFUL!' && sleep3 \""))
   ;;;~ set custom init file 
+  (setq user-init-file "~/Projects/dot-emacs/init-openbox.el")
   ;;;~ open the custom init file
   (find-file user-init-file)
   ;;;~ load other init files:
   ;;;~  * basic configuration
   (load-file "~/Projects/dot-emacs/init-essentials.el")
+  ;; (find-file "~/Projects/dot-emacs/init-essentials.el")
   ;;;~  * show startup message
   (load-file "~/Projects/dot-emacs/init-startup-time.el")
+  ;; (find-file "~/Projects/dot-emacs/init-startup-time.el")
   ;;;~  * dont show startup statistics
   (remove-hook 'emacs-startup-hook 'use-package-report)
   ;;;~ customize scratch buffer (didn't work)
   ;; (setq initial-major-mode 'sh-mode); originally emacs-lisp-mode
   ;;;~ buffer to show after starting emacs
-  ;; (setq initial-buffer-choice user-init-file)
   )
 
 ;;;~ FRAME 2: open openbox configuration files in new frame
 (progn
   (make-frame)
+  ;; (switch-to-buffer "*scratch*")
   (other-frame 1)
   ;;;~ config files
   ;; (find-file "~/.config/openbox/rc.xml")
+  (find-file "~/Projects/dot-emacs/src-org/init-openbox.org")
+  (find-file "~/Projects/archlinux/desktop/openbox/autostart"))
   (find-file "~/Projects/archlinux/desktop/openbox/shortcuts-openbox.sh")
   ;; (find-file "~/Projects/archlinux/desktop/openbox/conky.org")
-  (find-file "~/Projects/archlinux/desktop/openbox/autostart")
 
 ;;;~ FRAME 3:
-  (progn
-    ;; (add-hook 'emacs-startup-hook
-    ;; 	    #'(lambda ()
-    ;; 		(interactive)
-    (make-frame)
-    (other-frame 1)
-    (other-frame 1)
-    (modify-frame-location-upper-right)
-    (switch-to-buffer "*scratch*")
-    (find-file "~/Projects/")
-    (other-frame 1)
-    ))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(progn
+  (make-frame)
+  (other-frame 2)
+  (modify-frame-location-upper-right)
+  (other-frame 1)
+  (add-hook 'emacs-startup-hook
+	    #'(lambda ()
+		(interactive)
+		(find-file "~/Projects/")))
+  )
